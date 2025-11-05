8b956dc319b7ec7554ba9ca73b0f30979376a89b
John Su <Johnchangsu@gmail.com>
2025-11-05 12:25:49 +1100

    Added Faces method to calculate internal and boundary faces

--- examples/LBM/LDC.py
+++ examples/LBM/LDC.py
@@ -1,26 +1,61 @@
 from pde_module.stencils.LBM.kernels import create_streaming_kernel,create_BGK_kernel
 from pde_module.stencils.LBM.modules.gridBoundary import GridBoundary
 import warp as wp
-from pde_module.grids.cell_grid import UniformCellGrid
-
+from pde_module.grids import Grid
+import numpy as np
 wp.config.mode = 'debug'
 wp.init()
 
-LBM = UniformCellGrid(0.1,2,2)
-LBM.LBM()
+
+class D2Q9_Model:
+    def __init__(self):
+        self.num_discrete_velocities = 9
+        
+        self.D2Q9_velocities = np.array([
+            [ 0,  1,  0, -1,  0,  1, -1, -1,  1,],
+            [ 0,  0,  1,  0, -1,  1,  1, -1, -1,]
+        ]).swapaxes(0,1)
+        self.velocity_directions_int =wp.mat(shape = (self.num_discrete_velocities,self.dimension),dtype = int)(self.D2Q9_velocities)
+        
+        self.velocity_directions_float = wp.mat(shape = (self.num_discrete_velocities,self.dimension),dtype = float)(self.D2Q9_velocities.astype( self.float_dtype))
+        
+        
+        self.indices = wp.vec(length = self.num_discrete_velocities,dtype = int)(np.arange(self.num_discrete_velocities,dtype= int))
+        
+        self.weights = wp.vec(length = self.num_discrete_velocities,dtype = wp.dtype_from_numpy(self.float_dtype) )([
+                                4/9,                        # Center Velocity [0,]
+                                1/9,  1/9,  1/9,  1/9,      # Axis-Aligned Velocities [1, 2, 3, 4]
+                                1/36, 1/36, 1/36, 1/36,     # 45 ° Velocities [5, 6, 7, 8]
+                                ])
 
 
-f_old = LBM.generate_distribution_field()
-f_new = LBM.generate_distribution_field()
-vel_field = LBM.generate_velocity_field()
-rho_field = LBM.generate_density_field()
+n = 101 # Use Odd number of points!
+dx = 1/(n-1)
+LBM = Grid('cell',dx=dx,nx = n,ny = n,levels = 1)
+D2Q9 = D2Q9_Model()
+t = 0
 
-BGK_kernel = create_BGK_kernel(LBM.dimension,LBM.num_discrete_velocities)
-stream_kernel = create_streaming_kernel(LBM.dimension,LBM.num_discrete_velocities)
+dx = LBM.dx
+Re = 100.
+viscosity = 1/Re
+beta = 0.7
+CFL_LIMIT = 0.5
+
+dt = min(CFL_LIMIT*float(dx**2/(viscosity)),CFL_LIMIT*dx/(1+1/beta))
+print(f'{dt=:.3E} {beta=:.3E} {viscosity=:.3E}')
 
 
-wp.launch(BGK_kernel,dim = LBM.batch_shape(1),inputs=[f_old,rho_field,vel_field,LBM.velocity_directions_float,LBM.weights,0.5],outputs=[f_new])
-wp.launch(stream_kernel,dim = LBM.batch_shape(1),inputs=[f_old,LBM.velocity_directions_int],outputs=[f_new])
+f_old = LBM.create_cell_field(9)
+f_new = LBM.create_cell_field(9)
+vel_field = LBM.create_cell_field(3)
+rho_field = LBM.create_cell_field(1)
+
+BGK_kernel = create_BGK_kernel(LBM.dimension,D2Q9.num_discrete_velocities)
+stream_kernel = create_streaming_kernel(LBM.dimension,D2Q9.num_discrete_velocities)
+
+
+wp.launch(BGK_kernel,dim = ,inputs=[f_old,rho_field,vel_field,D2Q9.velocity_directions_float,D2Q9.weights,0.5],outputs=[f_new])
+wp.launch(stream_kernel,dim = ,inputs=[f_old,D2Q9.velocity_directions_int],outputs=[f_new])
 
 
 a = GridBoundary(LBM)
--- src/pde_module/grids/faces.py
+++ src/pde_module/grids/faces.py
@@ -19,15 +19,35 @@ class Faces():
         self.dx = dx
 
         self.face_centroid_limits = tuple(  np.linspace(o + self.dx/2.,o+self.dx*(s-1) - self.dx/2,s) if s > 1 else np.array([o]) for o,s in zip(self.grid_origin,self.grid_cell_shape))
-
-
+        self.num_cells = prod(self.grid_cell_shape)
+        self.cellIDs = np.arange(self.num_cells).reshape(self.grid_cell_shape)
+        
+        self.boundary_groups = dict()    
+        
+        
+        
+        self.ownnerNeighbors = -1*np.ones(shape =(self.num_faces,2),dtype = np.int32)
+        self.normals_mapping = np.empty(shape=self.num_faces,dtype=np.float32)
+        self.normals = np.zeros(shape = (2*self.dimension,self.dimension),dtype= np.float32)
+        
+        
+        
+        
     @property
     def area(self):
         return self.dx**self.dimension
     
     @property
     def num_faces(self):
-        return self.num_boundary_faces + self.num_internal_faces
+        num_faces = 0
+        
+        for axis in range(self.dimension):
+            faces_shape = list(self.grid_cell_shape)
+            faces_shape[axis] +=1
+            num_faces += prod(faces_shape)
+            
+        
+        return num_faces
     
     @property
     def num_boundary_faces(self):
@@ -40,38 +60,53 @@ class Faces():
 
         return num_faces
     
-    
-    def calculate_boundary_Faces(self):        
-        boundary_face_owners = -1*np.ones(shape = (self.num_boundary_faces,3),dtype=np.int32)
+    @property
+    def num_internal_faces(self):
+        return self.num_faces - self.num_boundary_faces
         
         
+    def calculate_boundary_Faces(self):
+        
+        boundary_face_owners = self.ownnerNeighbors
+            
         initial = 0
-        for axis in range(self.dimension):
+        for axis,axis_Name in zip(range(self.dimension),['X','Y','Z']):
             faces_shape = list(self.grid_cell_shape)
             faces_shape[axis] = 1 
             num_faces = prod(faces_shape)
-            for fixed_point in [0,self.grid_cell_shape[axis]-1]:
-                indices = np.indices(faces_shape,dtype= np.int32)
-                indices = np.moveaxis(indices,0,-1).reshape(-1,3)
-                indices[:,axis] += fixed_point
+            
+            for fixed_point,side in zip([0,self.grid_cell_shape[axis]-1],['-','+']):
+                indices = [slice(None),slice(None),slice(None)]
+                indices[axis] = fixed_point
+                boundary_face_owners[initial:initial+num_faces,0] = self.cellIDs[tuple(indices)].squeeze() # tuple indexing (each element corresponds to element) is different to list indexing with numpy arrays
                 
-                boundary_face_owners[initial:initial+num_faces] = indices
+                group_name = side +axis_Name 
+                self.boundary_groups[group_name] = np.arange(0,num_faces) + initial
+                
                 initial += num_faces
         
-        self.boundary_cells,self.owners = np.unique(boundary_face_owners,return_inverse=True,axis = 0)
         
     def calculate_internal_Faces(self):
         
+        # internal_face_owners = -1*np.ones(shape = self.num_internal_faces,dtype= np.int32)
+        internal_face_owners = self.ownnerNeighbors
         
-        self.cell_indices = np.indices(self.grid_cell_shape,dtype=int).reshape(-1,3)
-        
+        intital = self.num_boundary_faces
         for axis in range(self.dimension):
             faces_shape = list(self.grid_cell_shape)
             faces_shape[axis] -= 1 
             num_faces = prod(faces_shape)
             
-            faces = np.zeros(shape = (num_faces,2),dtype= np.int32)
+            indices = [slice(None),slice(None),slice(None)]
+            indices[axis] = slice(0,-1)
             
+            internal_face_owners[intital:intital+num_faces,0] = self.cellIDs[tuple(indices)].squeeze().flatten()
+            indices[axis] = slice(1,None)
+            internal_face_owners[intital:intital+num_faces,1] = self.cellIDs[tuple(indices)].squeeze().flatten()
+            
+            intital += num_faces
+            
+
             
         
     def create_faces_field(self):
@@ -80,11 +115,16 @@ class Faces():
 if __name__ == '__main__':
     f = Faces((0.,0.,0.),dx = 0.5,grid_cell_shape=(3,3,1),dimension=2)
     
+    
     f.calculate_boundary_Faces()
-    # f.calculate_internal_Faces()
-    print(f.boundary_cells)
-    print(f.owners)
-    print()
+    f.calculate_internal_Faces()
+    print(f.ownnerNeighbors)
+    print(f.num_faces,f.num_boundary_faces,f.num_internal_faces)
+    # f.calculate_boundary_Faces()
+    # # f.calculate_internal_Faces()
+    # print(f.boundary_cells)
+    # print(f.owners)
+    # print()
     
     # Nx, Ny, Nz = (3,3,1)
 
