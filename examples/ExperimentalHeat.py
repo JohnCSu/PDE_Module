37b4b7eb98c92eccc89cad9a28556bcb0ebad53c
John Su <Johnchangsu@gmail.com>
2026-01-21 13:11:45 +1100

    Add boundary module and tested for 2D Heat Equation

--- examples/ExperimentalHeat.py
+++ examples/ExperimentalHeat.py
@@ -1,82 +1,111 @@
+
 import numpy as np
 import warp as wp
-
-
+from matplotlib import pyplot as plt
 from pde_module.experimental.grid import Grid
 from pde_module.experimental.laplacian import Laplacian
 from pde_module.experimental.time_integrators import ForwardEuler
+from pde_module.experimental.boundary import GridBoundary
 
 wp.init()
-wp.config.mode = "debug"
+# wp.config.mode = "debug"
 
 if __name__ == '__main__':
-    n = 3
+    n = 41
     L = 1
     dx = L/(n-1)
     # x,y = np.linspace(0,1,n),np.linspace(0,1,n)
-    grid = Grid(dx = 1/(n-1),num_points=(n,n,1),origin= (0.,0.,0.))
+    grid = Grid(dx = 1/(n-1),num_points=(n,n,1),origin= (0.,0.,0.),ghost_cells=1)
+
+    # u = grid.create_node_field(1,1)
+
+    IC = lambda x,y,z: (np.sin(np.pi*x)*np.sin(np.pi*y))
+    u =grid.initial_condition('node',IC)
     
+    # meshgrid = grid.create_meshgrid('node')
     
-    u = grid.create_node_field(1,1)
+    # plot_meshgrid = [m.squeeze() for m in meshgrid]
+    # plt.contourf(*plot_meshgrid[:2],u.squeeze())
+    # plt.show()
     
+    # dx = grid.dx
+    alpha = 0.1
+    dt = float(dx**2/(4*alpha))
     
-    
+    # Define Modules
+    BC = GridBoundary(u,1,dx)
+    BC.dirichlet_BC('ALL',0.)
+    euler_step = ForwardEuler(u.dtype)  
     lapl = Laplacian(1,grid.dx)
+
     lapl.setup(u)
-    
-    euler_step = ForwardEuler(u.dtype)
-    
-    
-    stencil =lapl(u)
-    u_next = euler_step(u,stencil,0.1)
-    
-    
-    
-    # IC = lambda x,y,z: (np.sin(np.pi*x)*np.sin(np.pi*y))
-    # initial_value =grid.initial_condition(IC)
-    
-    t = 0
-    
-    dx = grid.dx
-    dt = float(dx**2/(4*0.1))
+    BC.setup(u)
+    t= 0
+    for i in range(1000):
+        u2 = BC(u)
+        stencil =lapl(u2,alpha)
+        u_next = euler_step(u2,stencil,dt)
+        u = u_next
+        t+= dt
 
+        if i % 100 == 0:
+            print(f'Max u = {u.numpy().max()} at t = {t},iter = {i}')
     
-    # boundary = GridBoundary(grid,1,dynamic_array_alloc= False)
-    # boundary.dirichlet_BC('ALL',0.)
-    
-    # laplcian_stencil = Laplacian(grid,1,dynamic_array_alloc=False)
-    # time_step = ForwardEuler(grid,1,dynamic_array_alloc= False)
-    
-    # # with wp.ScopedCapture(device="cuda") as iteration_loop:
-    # np.set_printoptions(precision=2)
-    # input_values = initial_value
-    # print(input_values.numpy()[0,:,:,0,0])
-    
-    # for i in range(1001):
-    #     boundary_corrected_values = boundary(input_values)
-    #     # print(boundary_corrected_values.numpy()[0,:,:,0,0])        
-    #     laplace = laplcian_stencil(boundary_corrected_values,scale =0.1)
-    #     new_value = time_step(boundary_corrected_values,laplace,dt)
-    #     input_values = new_value
-
-    #     t += dt
-    #     if t > 1.:
-    #         break
         
-    # print(f't = {t:.3e} max value = {np.max(input_values.numpy().max()):.3E}, dt = {dt:.3E}')
+
+# 
+# 
+
+# # print(u.shape)
+# # print(u.numpy().squeeze())
+
+
+# print(BC.boundary_interior.squeeze())
+# print(u2.numpy().squeeze())
+
+
+
+
+# t = 0
+
+
+
+
+# boundary = GridBoundary(grid,1,dynamic_array_alloc= False)
+# boundary.dirichlet_BC('ALL',0.)
+
+# laplcian_stencil = Laplacian(grid,1,dynamic_array_alloc=False)
+# time_step = ForwardEuler(grid,1,dynamic_array_alloc= False)
+
+# # with wp.ScopedCapture(device="cuda") as iteration_loop:
+# np.set_printoptions(precision=2)
+# input_values = initial_value
+# print(input_values.numpy()[0,:,:,0,0])
+
+# for i in range(1001):
+#     boundary_corrected_values = boundary(input_values)
+#     # print(boundary_corrected_values.numpy()[0,:,:,0,0])        
+#     laplace = laplcian_stencil(boundary_corrected_values,scale =0.1)
+#     new_value = time_step(boundary_corrected_values,laplace,dt)
+#     input_values = new_value
+
+#     t += dt
+#     if t > 1.:
+#         break
     
-    # to_plot = grid.trim_ghost_values(input_values)
-    # u = to_plot[0,:,:,0]
-    # meshgrid = grid.meshgrid('node',False,for_plotting=True)
-    # meshgrid = [m.T for m in meshgrid]
-    # # print(f'max u {np.max(u):.3E}')
-    
-    # plt.quiver(*meshgrid[::-1],u.T,v.T)
-    # plt.show()
-    
-    
-    # plt.contourf(*meshgrid[::-1],u.T,cmap ='jet',levels = 100)
-    # plt.colorbar()
-    # plt.show()
-    
-    
+# print(f't = {t:.3e} max value = {np.max(input_values.numpy().max()):.3E}, dt = {dt:.3E}')
+
+# to_plot = grid.trim_ghost_values(input_values)
+# u = to_plot[0,:,:,0]
+# meshgrid = grid.meshgrid('node',False,for_plotting=True)
+# meshgrid = [m.T for m in meshgrid]
+# # print(f'max u {np.max(u):.3E}')
+
+# plt.quiver(*meshgrid[::-1],u.T,v.T)
+# plt.show()
+
+
+# plt.contourf(*meshgrid[::-1],u.T,cmap ='jet',levels = 100)
+# plt.colorbar()
+# plt.show()
+
--- src/pde_module/experimental/boundary.py
+++ src/pde_module/experimental/boundary.py
@@ -3,27 +3,195 @@ import warp as wp
 from warp.types import vector,matrix
 # from .types import *
 from .hooks import *
-
+import numpy as np
 
 class GridBoundary(ExplicitUniformGridStencil):
-    def __init__(self, inputs,num_ghost:int,dx, float_dtype = wp.float32):
-        super().__init__(inputs, inputs, dx, float_dtype)
-        self.num_ghost = num_ghost
+    def __init__(self,field,ghost_cells:int,dx):
+        
+        inputs = self.get_shape_from_dtype(field.dtype)
+        super().__init__(inputs ,inputs, dx, field.dtype._wp_scalar_type_)
+        assert type(ghost_cells) is int and ghost_cells > 0
+        self.ghost_cells = ghost_cells
+        
+        self.field_shape = self.field_shape_with_no_ghost_cells(field.shape,ghost_cells)
+        self.ghost_shape = field.shape
+
+        
+        self.define_boundary_xyz_indices()
+        self.define_groups()
+        self.define_values_and_masks()
+        self.define_interior_adjacency()
+        
+        
+    def define_boundary_xyz_indices(self,*args,**kwargs):
+        
+        boundary_xyz_indices = []
+        
+        for i in range(3):
+            if self.field_shape[i] == 1:
+                continue 
+            axis_limits = (0,self.field_shape[i]-1)
+            for axis_lim in axis_limits:
+                side = list(self.field_shape)
+                side[i] = 1
+                
+                indices = np.indices(side,dtype = np.int32)
+                indices = np.moveaxis(indices,0,-1).reshape(-1,3)
+                indices[:,i] = axis_lim
+                
+                for ax in range(3):
+                    if self.field_shape[ax] != 1 :
+                        indices[:,ax] += self.ghost_cells
+                # print(indices)
+                
+                
+                boundary_xyz_indices.append(indices)
+                
+        self.boundary_xyz_indices = np.unique(np.concat(boundary_xyz_indices,axis = 0,dtype=np.int32),axis = 0).astype(np.int32)
+        # print()
     
+    def define_groups(self,*args,**kwargs):
+        '''
+        TODO:
+        Can be optimized with numba so we only to one sweep through all boundary indices
+        '''
         
-    def get_boundary_indices(self,field):
-        self.ghost_shape = self.get_ghost_shape(field.shape,self.num_ghost)
+        self.groups = {}
         
+        self.boundary_indices = np.arange(len(self.boundary_xyz_indices),dtype = np.int32)
+        self.groups['ALL'] = self.boundary_indices
         
+        for axis,axis_name in enumerate(['X','Y','Z']):
+            if self.field_shape[axis] == 1:
+                continue
+            coords = self.field_shape[axis]
+            
+            axis_limits = [0 + self.ghost_cells,coords-1 + self.ghost_cells] # We have the shifts existing
+            
+            for axis_limit,parity in zip(axis_limits,['-','+']):
+                name = parity + axis_name
+                self.groups[name] = self.boundary_indices[self.boundary_xyz_indices[:,axis] == axis_limit]
         
-def create_boundary_kernel(float_dtype):
     
+    def define_values_and_masks(self,*args, **kwargs):
+        shape = (len(self.boundary_xyz_indices),) + self.input_dtype_shape
+        self.boundary_value = np.zeros(shape,dtype=wp.dtype_to_numpy(self.input_scalar_type))
+        self.boundary_type = np.zeros_like(self.boundary_value,dtype = np.int8)
+
+    
+    def define_interior_adjacency(self):
+        self.boundary_interior = np.zeros(shape = (len(self.boundary_xyz_indices),3),dtype = np.int32)
+        for i,axis in enumerate(['X','Y','Z']):
+            if self.field_shape[i] == 1:
+                continue
+            for parity in ['-','+']:
+                sign = 1 if parity == '-' else -1
+                key = parity+axis
+                index = self.groups[key]
+                self.boundary_interior[index,i] = sign
+        
+        
+    
+    def set_BC(self,face_ids:str|int|np.ndarray|list|tuple,value:float,boundary_type:int,outputs_ids:int|np.ndarray|list|tuple|None):
+        if isinstance(face_ids,str):
+            assert face_ids in self.groups.keys()
+            face_ids = self.groups[face_ids]
+            
+        assert isinstance(face_ids,(np.ndarray,list,tuple,int))
+        
+        assert isinstance(outputs_ids,(int,list,tuple,np.ndarray)) or outputs_ids is None
+        
+        if isinstance(outputs_ids,int):
+            assert 0 <= outputs_ids < self.num_inputs
+        elif isinstance(outputs_ids,(list,tuple,np.ndarray)):
+            output_ids = np.array(output_ids,dtype = np.int32)
+            assert np.all( 0 <= output_ids < self.num_inputs)
+        
+        if outputs_ids is None:
+            outputs_ids = slice(None)
+        
+        assert isinstance(value,float), 'Value must be type float'
+        
+        self.boundary_type[face_ids,outputs_ids] = boundary_type # For Dirichlet
+        self.boundary_value[face_ids,outputs_ids] = value
+        
+    def dirichlet_BC(self,group:str|int|np.ndarray|list|tuple,value:float,outputs_ids:int|np.ndarray|list|tuple|None = None):
+        self.set_BC(group,value,0,outputs_ids)        
+    
+    def vonNeumann_BC(self,group:str|int|np.ndarray|list|tuple,value:float,outputs_ids:int|np.ndarray|list|tuple|None = None):
+        self.set_BC(group,value,1,outputs_ids)
+        
+        
+    @setup
+    def to_warp(self,*args,**kwargs):
+        self.warp_boundary_xyz_indices = wp.array(self.boundary_xyz_indices,dtype=wp.vec3i)
+        # self.warp_boundary_indices = wp.array(self.boundary_indices)
+        self.warp_boundary_interior =wp.array(self.boundary_interior,dtype = wp.vec3i)
+        self.warp_boundary_type =wp.array(self.boundary_type)
+        self.warp_boundary_value = wp.array(self.boundary_value,dtype = self.input_dtype)
+        
+        
+    @setup
+    def initialize_kernel(self, input_array, *args, **kwargs):
+        self.kernel = create_boundary_kernel(self.input_dtype,self.ghost_cells,self.dx)
+    
+    def forward(self, input_array, *args, **kwargs):
+        wp.copy(self.output_array,input_array)
+        wp.launch(
+            kernel=self.kernel,
+            dim = (len(self.boundary_indices),*self.input_dtype_shape),
+            inputs=[
+                input_array,
+                self.warp_boundary_xyz_indices,
+                self.warp_boundary_value,
+                self.warp_boundary_type,
+                self.warp_boundary_interior,
+            ],
+            outputs=[
+                self.output_array
+            ]
+        )
+        return self.output_array
+def create_boundary_kernel(input_dtype,ghost_cells,dx):
+    dx = dx
+    DIRICHLET = wp.int8(0)
+    VON_NEUMANN = wp.int8(1)
     @wp.kernel
-    def boundary(
+    def boundary_kernel(
         current_values:wp.array3d(dtype = input_dtype),
-        dx:float_dtype,
+        boundary_xyz_indices:wp.array(dtype=wp.vec3i),
+        boundary_value:wp.array(dtype = input_dtype),
+        boundary_type:wp.array2d(dtype = wp.int8),
+        boundary_interior:wp.array(dtype=wp.vec3i),
         new_values:wp.array3d(dtype = input_dtype),
         ):
         
-        pass
+        i,var = wp.tid() #Boundary Index
+        
+        nodeID = boundary_xyz_indices[i]
+        x = nodeID[0]
+        y = nodeID[1]
+        z = nodeID[2]
+        
+        # wp.printf('%d,%d,%d,   %d,%d,%d,\n',x,y,z, nodeID[0],nodeID[1],nodeID[2])
+        interior_vec = boundary_interior[i] 
+        
+        # new_values[x,y,z][0] = 1.
+        # boundary_type[i][var] = 1.
+        
+        if boundary_type[i][var] == DIRICHLET:
+            new_values[x,y,z][var] =  boundary_value[i][var]
+            
+            for j in range(3):
+                if interior_vec[j] != 0:                    
+                    inc_vec = wp.vec3i(0,0,0)
+                    inc_vec[j] = interior_vec[j]
+                    ghostID = nodeID - inc_vec
+                    adjID = nodeID + inc_vec
+                    new_values[ghostID[0],ghostID[1],ghostID[2]][var] =  type(dx)(2.)*boundary_value[i][var] - current_values[adjID[0],adjID[1],adjID[2]][var]
+                    
+                    
+            # for j in range(wp.static(ghost_cells)):
+    return boundary_kernel
+        
         
