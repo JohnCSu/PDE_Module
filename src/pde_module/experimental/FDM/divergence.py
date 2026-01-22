74b235f416521979c3762ffc0cab047b9e28fb35
John Su <Johnchangsu@gmail.com>
2026-01-22 16:23:50 +1100

    Changed inputs to not be reliant on initial field but back to inputs and field shape

--- src/pde_module/experimental/FDM/divergence.py
+++ src/pde_module/experimental/FDM/divergence.py
@@ -1,32 +1,34 @@
-# from .stencil import Stencil
 from .ExplicitUniformGridStencil import ExplicitUniformGridStencil
 import warp as wp
 from warp.types import vector,matrix,type_is_vector,type_is_matrix,types_equal
-# from .types import *
 from ..Stencil.hooks import *
 from pde_module.experimental.stencil_utils import create_stencil_op,eligible_dims_and_shift
 
-class Laplacian(ExplicitUniformGridStencil):
+
+
+class Divergence(ExplicitUniformGridStencil):
     '''
-    Calculate Divergence using uniform central differences
+    Calculate Divergence of a vector field to a scalar field (vector field of size 1)
     '''
-    def __init__(self,inputs,dx:float,ghost_cells,stencil = None, float_dtype=wp.float32):
+    def __init__(self,field_shape,dx:float,ghost_cells,stencil = None, float_dtype=wp.float32):
         
         if stencil is None:
-            self.stencil = wp.types.vector(3,dtype = float_dtype)([1./dx**2,-2./dx**2,1/dx**2])
+            self.stencil = wp.types.vector(3,dtype = float_dtype)([-1./(2*dx),0.,1/(2*dx)])
         else:
             raise ValueError('Custom stencil not implemented yet')        
         assert (self.stencil._length_ % 2) == 1,'stencil must be odd sized'
+
+        dimension = self.calculate_dimension_from_field_shape(field_shape)
         
-        super().__init__( inputs,inputs,dx,ghost_cells,float_dtype=float_dtype)
+        super().__init__(dimension, 1,dx,ghost_cells,float_dtype=float_dtype)
         
     @setup
     def initialize_kernel(self,input_array,*args, **kwargs):
         assert types_equal(self.input_dtype,input_array.dtype)
-        assert len(self.inputs) == 1,'Laplacian Only For Vectors'
-        self.kernel = create_Laplacian_kernel(self.input_dtype,input_array.shape,self.stencil,self.ghost_cells)
+        assert type_is_vector(self.input_dtype)
+        
+        self.kernel = create_Divergence_kernel(self.input_dtype,self.output_dtype,input_array.shape,self.stencil,self.ghost_cells)
         self.kernel_dim = self.field_shape_with_no_ghost_cells(input_array.shape,self.ghost_cells)
-    
     
     def forward(self, input_array,alpha = 1.,*args,**kwargs):    
         wp.launch(self.kernel,dim = self.kernel_dim,inputs = [
@@ -36,9 +38,8 @@ class Laplacian(ExplicitUniformGridStencil):
         outputs= [self.output_array])
         return self.output_array
     
-
-
-def create_Laplacian_kernel(input_vector,grid_shape,stencil,ghost_cells):
+    
+def create_Divergence_kernel(input_vector,output_vector,grid_shape,stencil,ghost_cells):
     '''
     We need to ensure num_inputs == num_outputs
     '''
@@ -49,12 +50,12 @@ def create_Laplacian_kernel(input_vector,grid_shape,stencil,ghost_cells):
     stencil_op = create_stencil_op(input_vector,stencil,ghost_cells)
     dims,dims_shift = eligible_dims_and_shift(grid_shape,ghost_cells) 
     
+    assert output_vector._length_ == 1
+    
     @wp.kernel
-    def laplacian_kernel(
-                        input_values:wp.array3d(dtype = input_vector),
+    def Divergence_kernel(input_values:wp.array3d(dtype = input_vector),
                         alpha:input_vector._wp_scalar_type_,
-                        output_values:wp.array3d(dtype = input_vector),
-                        ):
+                        output_values:wp.array3d(dtype = output_vector),):
         
         i,j,k = wp.tid() # Lets only do internal grid points
         
@@ -62,13 +63,13 @@ def create_Laplacian_kernel(input_vector,grid_shape,stencil,ghost_cells):
         index = wp.vec3i(i,j,k) 
         index += dims_shift
         
-        laplace = input_vector() # Vector same length as input array vec
+        div = output_vector() # Vector same length as input array vec
+        # Can do more effecient on per val but for now lets resues stencil op
         for i in range(wp.static(len(dims))):
-            laplace += stencil_op(input_values,index,stencil,dims[i])    
+            div[0] += stencil_op(input_values,index,stencil,dims[i])[i]    
         
-        laplace *= alpha
+        div *= alpha
         
-        output_values[index[0],index[1],index[2]] = laplace
-        
-    return laplacian_kernel
-        
+        output_values[index[0],index[1],index[2]] = div
+
+    return Divergence_kernel
---
+++ src/pde_module/experimental/FDM/grad.py
@@ -0,0 +1,79 @@
+from .ExplicitUniformGridStencil import ExplicitUniformGridStencil
+import warp as wp
+from warp.types import vector,matrix,type_is_vector,type_is_matrix,types_equal
+from ..Stencil.hooks import *
+from pde_module.experimental.stencil_utils import create_stencil_op,eligible_dims_and_shift
+
+
+class Grad(ExplicitUniformGridStencil):
+    '''
+    Calculate gradient of a scalar vector or the jacobian of a vector
+    '''
+    def __init__(self,inputs,field_shape,dx:float,ghost_cells,force_matrix:bool = False, float_dtype=wp.float32):
+        
+        self.force_matrix = force_matrix
+        dimension = self.calculate_dimension_from_field_shape(field_shape)
+        self.stencil = wp.types.vector(3,dtype = float_dtype)([-1./(2*dx),0.,1/(2*dx)])
+        
+        assert type(inputs) is int and inputs > 0
+        
+        if inputs == 1 and force_matrix is False:
+            output_shape = dimension
+        else:
+            output_shape = (inputs,dimension)
+        
+        assert dimension <= 3
+        
+        super().__init__(1,output_shape,dx,ghost_cells,float_dtype)
+        
+    @setup
+    def initialize_kernel(self,input_array,*args, **kwargs):
+        assert types_equal(self.input_dtype,input_array.dtype)
+        assert len(self.inputs) == 1,'Laplacian Only For Vectors'
+        self.kernel = create_Grad_kernel(self.input_dtype,self.output_dtype,input_array.shape,self.stencil,self.ghost_cells)
+        self.kernel_dim = self.field_shape_with_no_ghost_cells(input_array.shape,self.ghost_cells)
+    
+    def forward(self, input_array,alpha = 1.,*args,**kwargs):    
+        wp.launch(self.kernel,dim = self.kernel_dim,inputs = [
+            input_array,
+            alpha,   
+        ],
+        outputs= [self.output_array])
+        return self.output_array
+    
+
+
+def create_Grad_kernel(input_vector:vector,output_dtype,grid_shape,stencil,ghost_cells):
+    assert type_is_vector(input_vector)
+    
+    output_type = 'vector' if hasattr(output_dtype,'_length_') else 'matrix'
+    assert type_is_vector(output_dtype) or type_is_matrix(output_dtype)
+    
+    
+    stencil_op = create_stencil_op(input_vector,stencil,ghost_cells)
+    dims,dims_shift = eligible_dims_and_shift(grid_shape,ghost_cells)
+    @wp.kernel
+    def grad_kernel(
+        scalar_array:wp.array3d(dtype = input_vector),
+        alpha:input_vector._wp_scalar_type_,
+        grad_array:wp.array3d(dtype = output_dtype), 
+    ):
+        i,j,k = wp.tid() # Lets only do internal grid points
+        
+        # Step 1. Shift to adjust for ghost cells
+        index = wp.vec3i(i,j,k) 
+        index += dims_shift
+        
+        
+        grad = output_dtype()
+        
+        for i in range(wp.static(len(dims))):
+            if wp.static(output_type == 'vector'):
+                grad[i] = stencil_op(scalar_array,index,stencil,dims[i])[0] # Scalar value
+            else:
+                grad[:,i] = stencil_op(scalar_array,index,stencil,dims[i]) # Vector of derivatives of all outputs wrt to some axis
+
+        grad *= alpha
+        
+        grad_array[index[0],index[1],index[2]] = grad
+    return grad_kernel
