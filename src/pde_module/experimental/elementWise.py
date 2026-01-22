8ed74fecdcb7a656bd16b7a323601dac9b62840c
John Su <Johnchangsu@gmail.com>
2026-01-22 16:38:13 +1100

    improved ElementWise ops script

--- src/pde_module/experimental/elementWise.py
+++ src/pde_module/experimental/elementWise.py
@@ -1,7 +1,7 @@
-from .stencil import Stencil
+from .Stencil.stencil import Stencil
 import warp as wp
 from warp.types import vector,matrix,type_is_vector,type_is_matrix
-from .hooks import *
+from .Stencil.hooks import *
 from pde_module.experimental.stencil_utils import create_stencil_op,eligible_dims_and_shift
 
 
@@ -10,20 +10,21 @@ class ElementWise(Stencil):
     '''
     Base_class for Element wise operations
     '''
-    def __init__(self,element_op,output_dtype):
+    def __init__(self,element_op,output_dtype,float_dtype = wp.float32):
         self.element_op = element_op
         self._output_dtype
-        # super().__init__(dimension, 1,dx,ghost_cells,float_dtype=float_dtype)
+        self.float_dtype = float_dtype
         
-    @setup
+    @setup(order = 1)
     def initialize_kernel(self,array_A,array_B,*args, **kwargs):
+        super().__init__(array_A.dtype,self._output_dtype,0,0,self.float_dtype)
+        self.array_B_dtype =array_B.dtype
         
-        assert len(self.inputs) == 1,'Laplacian Only For Vectors'
-        
-        self.kernel = create_ElementOp_kernel(self.element_op,array_A.dtype,array_B.dtype,)
-        
+        self.kernel = create_ElementOp_kernel(self.element_op,array_A.dtype,array_B.dtype,self.output_dtype)
+        self.create_output_array(array_A)
     
-    def forward(self, array_A,array_B,*args,**kwargs):    
+    
+    def forward(self, array_A:wp.array,array_B:wp.array,*args,**kwargs):    
         dim = array_A.flatten().shape
         wp.launch(self.kernel,dim = dim,inputs = [
             array_A.flatten(),
