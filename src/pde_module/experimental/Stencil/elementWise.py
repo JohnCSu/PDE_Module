43920c632aae923b8c0c221da8d8a960d4ef9600
John Su <Johnchangsu@gmail.com>
2026-01-30 15:52:34 +1100

    Rewrote Elementwise to ensure super().__init__ is called in __init__ as well and created simple test to ensure working

--- src/pde_module/experimental/Stencil/elementWise.py
+++ src/pde_module/experimental/Stencil/elementWise.py
@@ -1,37 +1,38 @@
-from .Stencil.stencil import Stencil
+from .stencil import Stencil
 import warp as wp
-from warp.types import vector,matrix,type_is_vector,type_is_matrix
-from .Stencil.hooks import *
+from warp.types import vector,matrix,type_is_vector,type_is_matrix,types_equal
+from .hooks import *
 from pde_module.experimental.stencil_utils import create_stencil_op,eligible_dims_and_shift
+
+
 
 
 class ElementWise(Stencil):
     '''
     Base_class for Element wise operations between two arrays of same size
     '''
-    def __init__(self,element_op,output_dtype,float_dtype = wp.float32):
+    def __init__(self,element_op,output_dtype,float_dtype = wp.float32,debug = False):
         self.element_op = element_op
-        self._output_dtype
         self.float_dtype = float_dtype
-        
-        
+        super().__init__(output_dtype,output_dtype,self.float_dtype,debug = False)
+            
     @setup(order = 1)
     def initialize_kernel(self,array_A,array_B,*args, **kwargs):
-        assert array_A.shape == array_B.shape, 'input arrays must be the same!'
-        super().__init__(array_A.dtype,self._output_dtype,0,0,self.float_dtype)
+        assert array_A.shape == array_B.shape, 'input arrays must be the same Shape!'
         self.array_B_dtype =array_B.dtype
+        self.array_A_dtype =array_A.dtype
         
         self.kernel = create_ElementOp_kernel(self.element_op,array_A.dtype,array_B.dtype,self.output_dtype)
-        self.create_output_array(array_A)
-    
-    
+        self.output_array = self.create_output_array(array_B)
+        
     def forward(self, array_A:wp.array,array_B:wp.array,*args,**kwargs):    
-        dim = array_A.flatten().shape
+        dim = array_A.size
+        x,y = array_A.flatten(),array_B.flatten()
+        z = self.output_array.flatten()
         wp.launch(self.kernel,dim = dim,inputs = [
-            array_A.flatten(),
-            array_B.flatten(),   
+               x,y
         ],
-        outputs= [self.output_array])
+        outputs= [z])
         return self.output_array
 
 
@@ -42,10 +43,9 @@ def create_ElementOp_kernel(element_op,array_A_dtype,array_B_dtype,output_array_
                            B:wp.array(dtype=array_B_dtype),
                            output_array:wp.array(dtype = output_array_dtype)):
         i = wp.tid()
-        output_array[i] =  element_op(A[i],B[i])
+        output_array[i] = element_op(A[i],B[i])
         
-
-    
+        
     return elementWise_kernel
         
         
---
+++ tests/elementWise.py
@@ -0,0 +1,46 @@
+import numpy as np
+import warp as wp
+from matplotlib import pyplot as plt
+from pde_module.experimental.grid import Grid
+from pde_module.experimental.FDM.elementWiseOps import scalarVectorMult,scalarVectorMultiply
+
+def create_ElementOp_kernel(element_op,array_A_dtype,array_B_dtype,output_array_dtype):
+    @wp.kernel
+    def elementWise_kernel(A:wp.array(dtype=array_A_dtype),
+                           B:wp.array(dtype=array_B_dtype),
+                           output_array:wp.array(dtype = output_array_dtype)):
+        i = wp.tid()
+        output_array[i] = element_op(A[i],B[i])
+        
+        
+    return elementWise_kernel
+        
+        
+
+
+if __name__ == '__main__':
+    wp.init()
+    wp.config.mode = "debug"
+
+    n = 21
+    L = 1
+    dx = L/(n-1)
+    ghost_cells = 1
+    # x,y = np.linspace(0,1,n),np.linspace(0,1,n)
+    grid = Grid(dx = 1/(n-1),num_points=(n,n,1),origin= (0.,0.,0.),ghost_cells=ghost_cells)
+    
+    u = grid.create_node_field(2)
+    u.fill_(wp.vec2f(2.,2.))
+    p = grid.create_node_field(1)
+    p.fill_(3.)
+    out = grid.create_node_field(2)
+    
+    momentum = scalarVectorMult(u.dtype)
+    out = momentum(p,u)
+    
+    # op = scalarVectorMultiply(out.dtype)
+    # kernel = create_ElementOp_kernel(op,p.dtype,u.dtype,u.dtype)
+    # wp.launch(kernel,dim = u.size,inputs=[p.flatten(),u.flatten()],outputs=[out.flatten()])
+    
+    print(out.numpy().flatten()[0])
+    assert np.all(np.isclose(out.numpy().flatten(),6.))
