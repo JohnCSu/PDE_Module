535e66a0fa4b8b8e0e1f2c721ac4868fa8568b61
John Su <Johnchangsu@gmail.com>
2026-01-30 15:54:46 +1100

    Added Element wise ops namely Outer Product of two vector fields and scalarVectorMult which multiplies a scalar vector field with a normal vector field

---
+++ src/pde_module/experimental/FDM/elementWiseOps.py
@@ -0,0 +1,51 @@
+from ..Stencil.elementWise import ElementWise
+
+import warp as wp
+from warp.types import vector,matrix,types_equal
+from ..Stencil.hooks import *
+
+
+def scalarVectorDiv(scalar_field,vector_field):
+    scalar_dtype = scalar_field.dtype
+    scalar_float = scalar_dtype._wp_scalar_type_
+    return scalar_float(1.)/scalar_field.view(scalar_float) * vector_field
+
+def scalarVectorMulti(scalar_field,vector_field):
+    scalar_dtype = scalar_field.dtype
+    return scalar_field.view(scalar_dtype._wp_scalar_type_) * vector_field
+
+
+
+class OuterProduct(ElementWise):
+    '''
+    Compute outer product between 2 vectors and output as matrix
+    '''
+    def __init__(self, vec_A:int,vec_B:int, float_dtype=wp.float32):
+        element_op = wp.outer
+        output_dtype = matrix((vec_A,vec_B),dtype = float_dtype)
+        super().__init__(element_op, output_dtype, float_dtype)
+        self.vec_A = vector(vec_A,float_dtype)
+        self.vec_B = vector(vec_B,float_dtype)
+    @setup(order = 2)
+    def check_same(self,array_A,array_B,*args, **kwargs):
+        assert types_equal(array_A.dtype,self.vec_A) and types_equal(array_B.dtype,self.vec_A)
+        
+
+
+
+class scalarVectorMult(ElementWise):
+    '''
+    Multiply a scalar field (i.e. vector with length 1) with a corresponding vector/matrix field (which can be arbitary).
+    '''
+    def __init__(self, output_dtype, float_dtype=wp.float32):
+        element_op = scalarVectorMultiply(output_dtype)
+        super().__init__(element_op, output_dtype, float_dtype)
+
+
+def scalarVectorMultiply(output_dtype):
+    float_type = output_dtype._wp_scalar_type_
+    @wp.func
+    def _scalarVectorMultiply(a:vector(1,float_type),b:output_dtype):
+        return a[0]*b
+        
+    return _scalarVectorMultiply
