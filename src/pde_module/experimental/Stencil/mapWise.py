c0371bfb2e375912e4607f707d0b0d04a030d303
John Su <Johnchangsu@gmail.com>
2026-02-02 11:10:03 +1100

    Added MapWise Stencil That is more general than elementWise

---
+++ src/pde_module/experimental/Stencil/mapWise.py
@@ -0,0 +1,34 @@
+from .stencil import Stencil
+import warp as wp
+from warp.types import vector,matrix,type_is_vector,type_is_matrix,types_equal
+from .hooks import *
+from pde_module.experimental.stencil_utils import create_stencil_op,eligible_dims_and_shift
+
+
+class MapWise(Stencil):
+    '''
+    Base_class for Map_wise operations. The first input must be an array and the output array
+    is assumed to be the same dtype and shape
+    '''
+    def __init__(self,element_op,float_dtype = wp.float32,debug = False):
+        self.element_op = element_op
+        self.float_dtype = float_dtype
+        super().__init__(None,None,self.float_dtype,debug = False)
+        
+    @setup(order = 1)
+    def initialize_kernel(self,array_A,*args, **kwargs):
+        self._input_dtype = array_A.dtype
+        self._output_dtype = array_A.dtype
+        
+        self.output_array = self.create_output_array(array_A)
+        self.kernel = wp.map(self.element_op,array_A,*args,out= self.output_array,return_kernel=True)
+        
+    def forward(self, array_A:wp.array,*args,**kwargs):    
+        dim = array_A.shape
+        wp.launch(self.kernel,dim = dim,inputs = [
+        array_A,*args       
+        ],
+        outputs= [self.output_array])
+        return self.output_array
+
+        
