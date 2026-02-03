3afab65de4a0dd69e47293a6e62dea9f308cce47
John Su <Johnchangsu@gmail.com>
2026-02-03 15:38:25 +1100

    Added BC and Fluid State Constants

--- src/pde_module/experimental/constants.py
+++ src/pde_module/experimental/constants.py
@@ -1,3 +1,14 @@
 import warp as wp
+
+#Int Maxs and mins
 INT32_MAX = wp.int32(2**31 -1)
 INT32_MIN = wp.int32(-2**31)
+
+# BC
+DIRICHLET = wp.int8(1)
+VON_NEUMANN = wp.int8(2)
+
+
+# Cell State
+FLUID_CELL = wp.int8(0)
+SOLID_CELL = wp.int8(1)
