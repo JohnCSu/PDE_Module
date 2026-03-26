dd3c719006fb4a4edeb24f4892244581260ac83b
John <JohnChangSu@gmail.com>
2026-03-26 12:28:50 +1100

    added function to convert numpy arrays to vector or mat type

--- src/pde_module/LBM/__init__.py
+++ src/pde_module/LBM/__init__.py
@@ -2,3 +2,4 @@ from .BGK import BGK_collision
 from .stream import Streaming
 from .lattticeModels.latticeModel import LatticeModel
 from .mesh import LBM_Mesh
+from .boundary import Boundary
--- src/pde_module/LBM/utils.py
+++ src/pde_module/LBM/utils.py
@@ -1,9 +1,14 @@
 import warp as wp
-from warp.types import vector,matrix
+from warp.types import vector,matrix,is_array,is_vector,is_matrix
 from pde_module.utils.dummy_types import wp_Array,wp_Vector,wp_Matrix
+import numpy as np
 
-def wp_array_to_vec_or_mat(wp_arr:wp_Array) -> wp_Vector|wp_Matrix:
-    arr = wp_arr.numpy()
+def array_to_vec_or_mat(wp_arr:wp_Array) -> wp_Vector|wp_Matrix:
+    if is_array(wp_arr):
+        arr = wp_arr.numpy()
+    else:
+        arr = np.array(wp_arr)
+        
     if len(arr.shape) == 1:
         return vector(len(arr),dtype = wp.dtype_from_numpy(arr.dtype))(arr)
     elif len(arr.shape) == 2:
@@ -12,3 +17,11 @@ def wp_array_to_vec_or_mat(wp_arr:wp_Array) -> wp_Vector|wp_Matrix:
         raise ValueError('only arrays that map to 1D or 2D numpy arrays can be used')
 
 
+
+def vec_or_mat_to_numpy(x):
+    if is_vector(x):
+        return np.array(x,dtype= wp.dtype_to_numpy(x._wp_scalar_type_))
+    elif is_matrix(x):
+        return np.array(x,dtype=wp.dtype_to_numpy(x._wp_scalar_type_)).reshape(x._shape_)
+    else:
+        raise TypeError('x must be warp vec or mat')
