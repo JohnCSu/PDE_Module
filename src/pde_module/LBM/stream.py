cca3dc2dc2fd10ac1eae8374e15c65f4b4144645
John <JohnChangSu@gmail.com>
2026-03-27 12:00:14 +1100

    Fixed adjacent Bug that had subtraction of velocity direction when it should be positive for better intuitive behaviour

--- src/pde_module/LBM/stream.py
+++ src/pde_module/LBM/stream.py
@@ -6,7 +6,7 @@ from pde_module.LBM.lattticeModels.latticeModel import LatticeModel
 from pde_module.stencil.hooks import *
 from pde_module.LBM.LBM_Stencil import LBM_Stencil
 from pde_module.utils import ijk_to_global_c,xijk_to_global_c
-
+from .utils import get_adjacent_ijk
 
 class Streaming(LBM_Stencil):
     grid_shape:tuple[int]
@@ -30,6 +30,8 @@ class Streaming(LBM_Stencil):
 def create_streaming_kernel(f_arr:wp_Array,int_directions:wp_Matrix,num_distributions:int,grid_shape:tuple[int],dimension:int):
     grid_shape = wp.vec3i(grid_shape)
     
+    adjacent_ijk = get_adjacent_ijk(dimension,grid_shape)
+    
     @wp.kernel
     def streaming_kernel(f_in:wp.array2d(dtype =f_arr.dtype),
                          f_out:wp.array2d(dtype =f_arr.dtype)):
@@ -37,18 +39,11 @@ def create_streaming_kernel(f_arr:wp_Array,int_directions:wp_Matrix,num_distribu
         
         global_id = ijk_to_global_c(i,j,k,grid_shape[0],grid_shape[1],grid_shape[2])
         for f in range(num_distributions):
-            
-            vel_dir = int_directions[f] # N,D so vel_dir is a vector of length D dtype int32
-            ni = (i - vel_dir[0] + grid_shape[0]) % grid_shape[0]
-            nj = (j - vel_dir[1] + grid_shape[1]) % grid_shape[1]
-            nk = (k - vel_dir[2] + grid_shape[2]) % grid_shape[2]
-            
+            vel_dir = -int_directions[f] # N,D so vel_dir is a vector of length D dtype int32
+            ni,nj,nk = adjacent_ijk(i,j,k,vel_dir)
             adj_id  = ijk_to_global_c(ni,nj,nk,grid_shape[0],grid_shape[1],grid_shape[2])
             f_out[f,global_id]=  f_in[f,adj_id]
             
     return streaming_kernel
-
-
-
 
 
