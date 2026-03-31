3766a9e55dab65e2f7b705389b58511ba498572c
John <JohnChangSu@gmail.com>
2026-03-31 17:00:35 +1100

    Fixed Equilibrium BC using the wrong direction

--- src/pde_module/LBM/boundary.py
+++ src/pde_module/LBM/boundary.py
@@ -7,7 +7,7 @@ from pde_module.stencil.hooks import *
 from pde_module.LBM.LBM_Stencil import LBM_Stencil
 from pde_module.utils import ijk_to_global_c,global_to_ijk_c
 from .utils import get_adjacent_ijk
-
+from .BGK import create_BGK_feq
 FLUID = 0
 SOLID_WALL = 1
 MOVING_WALL = 2
@@ -21,8 +21,10 @@ class Boundary(LBM_Stencil):
     def __init__(self, latticeModel, grid_shape):
         super().__init__(latticeModel, grid_shape)    
         self.flags = np.zeros(self.grid_shape,dtype=np.uint8)
-        self.BC_velocity = np.zeros(self.grid_shape + (self.dimension,),dtype= self.latticeModel.float_dtype)
-        self.BC_density = np.ones(self.grid_shape + (self.dimension,),dtype= self.latticeModel.float_dtype)
+        
+        
+        self.BC_velocity = np.full(self.grid_shape + (self.dimension,),np.nan,dtype= self.latticeModel.float_dtype)
+        self.BC_density = np.full(self.grid_shape,np.nan,dtype= self.latticeModel.float_dtype)
         
         
         self.groups ={
@@ -38,8 +40,11 @@ class Boundary(LBM_Stencil):
         }
         
 
-    def set_BC(self,ids:str | tuple[np.ndarray | int | slice],boundary_type,velocity_value = 0., density_value = 1.):
+    def set_BC(self,ids:str | tuple[np.ndarray | int | slice],boundary_type,velocity = None, density = None):
         
+        if boundary_type == 2 or boundary_type == 3:
+            assert (velocity is not None or density is not None)
+            
         match ids:
             case str():
                 assert ids in self.groups.keys()
@@ -51,8 +56,10 @@ class Boundary(LBM_Stencil):
                 raise TypeError('Strings or tuples of ndarrays are allowed')
                     
         self.flags[*ids] = boundary_type
-        self.BC_velocity[*ids,:] = velocity_value
-        self.BC_density[ids] = density_value
+        if velocity is not None:
+            self.BC_velocity[*ids,:] = velocity
+        if density is not None:
+            self.BC_density[ids] = density
         
     
     
@@ -91,6 +98,25 @@ class Boundary(LBM_Stencil):
 def create_boundary_kernel(num_distributions,opposite_index,int_directions,float_directions,weights,dimension,grid_shape,float_dtype):
     grid_shape = wp.vec3i(grid_shape)
     adjacent_ijk = get_adjacent_ijk(dimension,grid_shape)
+    BGK_feq = create_BGK_feq(dimension,float_dtype)
+    @wp.func
+    def get_adjacent_fluid(i:int,
+                        j:int,
+                        k:int,
+                        flags:wp.array3d(dtype = wp.uint8)):
+        idx_vec = vector(0,length = dimension)
+        shift = wp.vec2i(-1,1)
+        for ii in range(dimension): # Axis
+            for jj in range(2):
+                idx_vec[ii] = shift[jj]
+                ni,nj,nk= adjacent_ijk(i,j,k,idx_vec)
+                if flags[ni,nj,nk] == FLUID:
+                    # wp.printf('FLUID\n')
+                    return ni,nj,nk
+                idx_vec[ii] = 0 
+                
+        return i,j,k
+    
     
     @wp.kernel
     def apply_BC(f_in:wp.array2d(dtype =float_dtype),
@@ -106,26 +132,63 @@ def create_boundary_kernel(num_distributions,opposite_index,int_directions,float
         rho_wall = rho_BCs[tid]
         i,j,k = global_to_ijk_c(global_id,grid_shape[0],grid_shape[1],grid_shape[2])
         
+        fluid_i,fluid_j,fluid_k = get_adjacent_fluid(i,j,k,flags)
+        fluid_adj = ijk_to_global_c(fluid_i,fluid_j,fluid_k,grid_shape[0],grid_shape[1],grid_shape[2])
+        u = vector(float_dtype(0.),length = dimension) # Boundary u to use if u not assigned
+        # u = wp.vec2f()
+        rho = float_dtype(0.) # Rho to use if rho not assigned
+        for f in range(num_distributions):
+            rho += f_in[f,fluid_adj]
+            u += f_in[f,fluid_adj]*float_directions[f]
+        u /= rho
+        
+        
+        if wp.isnan(u_wall[0]):
+            u_wall = u
+        
+        if wp.isnan(rho_wall):
+            rho_wall = rho
+        
+        # if j == (grid_shape[1] - 1):
+        #     wp.printf('%f:.1f %d \n',rho_wall,j)
+            
+        # if flags[i,j,k] != EQUILIBRIUM:
         for f in range(num_distributions):
             opp_f = opposite_index[f]
             opp_vel = int_directions[opp_f]
             ni,nj,nk = adjacent_ijk(i,j,k,opp_vel)
-            # if : # adj node is fluid
             adj_is_fluid = (flags[ni,nj,nk] == FLUID)
-            curr_is_moving = (flags[i,j,k] == MOVING_WALL)
+            bc_is_moving = (flags[i,j,k] == MOVING_WALL)
+            bc_is_equil = (flags[i,j,k] == EQUILIBRIUM)
+
             
-            f_out[opp_f,global_id] = f_in.dtype(adj_is_fluid)*f_in[f,global_id] + f_in.dtype(not adj_is_fluid)*f_in[opp_f,global_id]
-            f_out[opp_f,global_id] -= f_in.dtype(adj_is_fluid and curr_is_moving)*2.*3.*weights[f]*rho_wall*wp.dot(float_directions[f],u_wall) 
+            new_opp_f = wp.where(flags[ni,nj,nk] == FLUID,f_in[f,global_id],f_in[opp_f,global_id]) # if opp dir is Fluiod Do Bounceback False basically do nothing
+            new_opp_f -= wp.where(adj_is_fluid and bc_is_moving,2.*3.*weights[f]*rho_wall*wp.dot(float_directions[f],u_wall),float_dtype(0.)) # If moving wall as well add forcing term
+            new_opp_f = wp.where(bc_is_equil,BGK_feq(weights[opp_f],rho_wall,u_wall,float_directions[opp_f]),new_opp_f) # If Equil BC, replace with equilibrium
+            f_out[opp_f,global_id] = new_opp_f
+        # else:
+        #     for f in range(num_distributions):
+        #         f_out[f,global_id] = 
+                # f_out[f,global_id] = f_out[f,fluid_adj]
+                
             # # The Above is equivalent to below (avoids if statements)            
             # if adj_is_fluid:
             #     f_out[opp_f,global_id] = f_in[f,global_id]
             #     if flags[i,j,k] == MOVING_WALL: # current is at a Moving Wall
             #         f_out[opp_f,global_id] -= 2.*3.*weights[f]*rho*wp.dot(float_directions[f],u_wall)
-                
+
+            
                 
                 
         
     return apply_BC
+
+
+
+
+            
+        
+    
 
 
 
