e6b03f3653433c8a129706747c1e22be9234e3d7
John <JohnChangSu@gmail.com>
2026-04-09 14:29:25 +1000

    adde BC String dict to helpwth defining funcgroup names

--- src/pde_module/FDM/boundary/flags.py
+++ src/pde_module/FDM/boundary/flags.py
@@ -8,12 +8,17 @@ INT32_MIN = wp.int32(-(2**31))
 NO_BC = 0
 """No boundary condition flag."""
 
+
 DIRICHLET = 1
 """Dirichlet boundary condition flag."""
 
 VON_NEUMANN = 2
 """Von Neumann boundary condition flag."""
 
+BC_SRING = {
+    1:'DIRICHLET',
+    2:'VON_NEUMANN'
+    }
 
 
 
