5abd114b1374421dff194b499dff3f0dd1e18205
John <JohnChangSu@gmail.com>
2026-04-09 12:54:29 +1000

    addeed gett unique key to utils import

--- src/pde_module/utils/__init__.py
+++ src/pde_module/utils/__init__.py
@@ -1,4 +1,4 @@
-from .utils import tuplify, dtype_from_shape, SignatureMismatchError
+from .utils import tuplify, dtype_from_shape, SignatureMismatchError,get_unique_key
 from .warp_utils import ijk_to_global_c,xijk_to_global_c,global_to_ijk_c
 from .types import (
     wp_Matrix,
