6b0bfb81657ef302ae6b6f351295585c765ec2e4
John <JohnChangSu@gmail.com>
2026-04-12 13:47:18 +1000

    Updated warp-lang package to 12.1

--- src/pde_module/FV/fluxes.py
+++ src/pde_module/FV/fluxes.py
@@ -1,4 +1,5 @@
 from pde_module.mesh import Mesh
+from pde_module.mesh.topology import *
 import numpy as np
 
 
@@ -12,5 +13,8 @@ For FV we need to know faces and the
 class FiniteVolumeMesh(Mesh):
     def __init__(self, nodes, cells_connectivity, cell_types, dimension = None, float_dtype=np.float32, int_dtype=np.int32):
         super().__init__(nodes, cells_connectivity, cell_types, dimension, float_dtype, int_dtype)
+        
+        
+        
         
         
