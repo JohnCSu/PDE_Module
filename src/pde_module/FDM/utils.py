9667f42848a9a84fa4ef0422054c376e5db1f5cb
John <JohnChangSu@gmail.com>
2026-04-12 13:46:53 +1000

    Marked stencil function for deprecation as no longer using local arrays to store values

--- src/pde_module/FDM/utils.py
+++ src/pde_module/FDM/utils.py
@@ -27,62 +27,62 @@ def eligible_dims_and_shift(
     )
 
 
-def create_stencil_over_axis_function(input_dtype : wp_Vector, stencil: wp_Vector):
-    """Create a stencil operation for finite difference calculations.
+# def create_stencil_over_axis_function(input_dtype : wp_Vector, stencil: wp_Vector):
+#     """Create a stencil operation for finite difference calculations.
 
-    Gathers neighbors of the current index along a given axis and applies
-    the stencil weights. Outputs a vector of the same type as input_vector.
+#     Gathers neighbors of the current index along a given axis and applies
+#     the stencil weights. Outputs a vector of the same type as input_vector.
 
-    Args:
-        input_dtype: Warp vector dtype for the input field.
-        stencil: Warp vector dtype containing stencil weights.
-        ghost_cells: Number of ghost cells in the grid.
+#     Args:
+#         input_dtype: Warp vector dtype for the input field.
+#         stencil: Warp vector dtype containing stencil weights.
+#         ghost_cells: Number of ghost cells in the grid.
 
-    Returns:
-        A wp.func that computes the stencil operation.
-    """
-    length = stencil._length_
-    assert (length % 2) == 1, "stencil must be odd sized"
+#     Returns:
+#         A wp.func that computes the stencil operation.
+#     """
+#     length = stencil._length_
+#     assert (length % 2) == 1, "stencil must be odd sized"
 
-    @wp.func
-    def apply_stencil_over_axis(
-        values:wp.array(dtype = input_dtype), # 2N+1 array with vector/matrix dtype
-    ):
-        output = input_dtype()
-        for i in range(length):
-            output += stencil[i]*values[i]
-        return output
-    return apply_stencil_over_axis
+#     @wp.func
+#     def apply_stencil_over_axis(
+#         values:wp.array(dtype = input_dtype), # 2N+1 array with vector/matrix dtype
+#     ):
+#         output = input_dtype()
+#         for i in range(length):
+#             output += stencil[i]*values[i]
+#         return output
+#     return apply_stencil_over_axis
 
 
-def create_n_point_stencil_function(stencil:wp_Vector,grid_shape:tuple[int],input_dtype:wp_Vector | wp_Matrix):
-    '''
-    Generalisation of the 'cross' point stencils e.g. Five point stenceil to n points
-    so if we have k points on either side of the central point then then we get an array of (D,2k)
-    '''
-    num_neighbors = (len(stencil)-1)//2
+# def create_n_point_stencil_function(stencil:wp_Vector,grid_shape:tuple[int],input_dtype:wp_Vector | wp_Matrix):
+#     '''
+#     Generalisation of the 'cross' point stencils e.g. Five point stenceil to n points
+#     so if we have k points on either side of the central point then then we get an array of (D,2k)
+#     '''
+#     num_neighbors = (len(stencil)-1)//2
 
-    dims,_ = eligible_dims_and_shift(grid_shape,0) # We just need the availiable dims
-    dimension = len(dims)
+#     dims,_ = eligible_dims_and_shift(grid_shape,0) # We just need the availiable dims
+#     dimension = len(dims)
     
-    shape = vector(dimension,dtype = int)(tuple(num_neighbors*2+1  for _ in dims))
+#     shape = vector(dimension,dtype = int)(tuple(num_neighbors*2+1  for _ in dims))
     
-    shift_mapping = vector(num_neighbors*2+1,dtype = int)(tuple(i for i in range(-num_neighbors, num_neighbors + 1)))
+#     shift_mapping = vector(num_neighbors*2+1,dtype = int)(tuple(i for i in range(-num_neighbors, num_neighbors + 1)))
     
-    array_shape = (dimension,num_neighbors*2+1)
+#     array_shape = (dimension,num_neighbors*2+1)
     
-    @wp.func
-    def get_neighbors(i:int,j:int,k:int,arr:wp.array3d(dtype = input_dtype)):
-        tmp = wp.zeros(shape = (wp.static(array_shape[0]),wp.static(array_shape[1])),dtype=input_dtype)
-        shift = wp.vec3i()
+#     @wp.func
+#     def get_neighbors(i:int,j:int,k:int,arr:wp.array3d(dtype = input_dtype)):
+#         tmp = wp.zeros(shape = (wp.static(array_shape[0]),wp.static(array_shape[1])),dtype=input_dtype)
+#         shift = wp.vec3i()
         
-        for ii in range(dimension): # Axis
-            for n in range(shape[ii]): # num neighbors in each
-                axis = dims[ii] # Get the corresponding axis
-                shift[axis] = shift_mapping[n]
-                tmp[ii,n] = arr[i+shift[0],j+shift[1],k+shift[2]]
-                shift[axis] = 0 # Reset Index shift
+#         for ii in range(wp.static(dimension)): # Axis
+#             for n in range(wp.static(shape[ii])): # num neighbors in each
+#                 axis = dims[ii] # Get the corresponding axis
+#                 shift[axis] = shift_mapping[n]
+#                 tmp[ii,n] = arr[i+shift[0],j+shift[1],k+shift[2]]
+#                 shift[axis] = 0 # Reset Index shift
         
-        return tmp
+#         return tmp
         
-    return get_neighbors
+#     return get_neighbors
