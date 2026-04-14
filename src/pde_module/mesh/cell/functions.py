307a20098f28c3e81e562f334ef5797c6439051c
John <JohnChangSu@gmail.com>
2026-04-14 15:29:01 +1000

    added Cell centroid and volume functions

--- src/pde_module/mesh/cell/functions.py
+++ src/pde_module/mesh/cell/functions.py
@@ -7,8 +7,8 @@ from pde_module.mesh.cell_types.cell_types import (
 )
 
 
-@nb.njit
-def _calculate_cell_centroids(
+@nb.njit(cache = True)
+def calculate_cell_centroids(
     nodes: np.ndarray, cells: np.ndarray, cell_offsets: np.ndarray
 ) -> np.ndarray:
     """Calculate unweighted centroids of each cell.
@@ -22,15 +22,52 @@ def _calculate_cell_centroids(
         Array of centroids with shape (n_cells, 3).
     """
     n_cells = cell_offsets.shape[0]
-    centroids = np.zeros((n_cells, 3), dtype=np.float32)
+    centroids = np.zeros((n_cells, 3), dtype=nodes.dtype)
 
-    for i in range(n_cells):
+    for i in nb.prange(n_cells):
         start_idx = cell_offsets[i]
         num_nodes = cells[start_idx]
-        cell_ids = cells[start_idx + 1 : start_idx + 1 + num_nodes]
-        node_locs = nodes[cell_ids, :]
-        centroids[i, :] = node_locs.mean(axis=0)
+        node_IDs = cells[start_idx + 1 : start_idx + 1 + num_nodes]
+        
+        cx = 0.
+        cy = 0.
+        cz = 0.
+        for j in range(num_nodes):
+            cx += nodes[node_IDs[j],0]
+            cy += nodes[node_IDs[j],1]
+            cz += nodes[node_IDs[j],2]
+            
+        centroids[i, 0] = cx/num_nodes
+        centroids[i, 1] = cy/num_nodes 
+        centroids[i, 2] = cz/num_nodes  
     return centroids
+
+
+@nb.njit(cache = True)
+def calculate_cell_volumes(cell_centroids,cell_to_face,cell_to_face_offset,face_centroids,face_normals,dimension):
+    volumes = np.empty(len(cell_centroids),dtype = cell_centroids.dtype)
+    
+    for cell_id in nb.prange(volumes.shape[0]):
+        offset = cell_to_face_offset[cell_id]
+        num_faces = cell_to_face[offset]
+        volume = 0.
+        centroid = cell_centroids[cell_id]
+        for i in range(1,num_faces+1):
+            face_id = cell_to_face[offset + i]
+            
+            face_centroid = face_centroids[face_id]
+            face_normal = face_normals[face_id]
+            
+            r = face_centroid - centroid
+            r_dot_f_n = np.dot(r,face_normal)
+            r_dot_f_n = np.where(r_dot_f_n < 0.,-1.,1.)*r_dot_f_n
+            volume += r_dot_f_n 
+        
+        volumes[cell_id] = volume/dimension
+    return volumes
+            
+            
+
 
 
 def check_cell_types(
