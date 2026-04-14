c5814b1f1395651a2e38ee8b531c5f1723496d9f
John <JohnChangSu@gmail.com>
2026-04-14 15:30:31 +1000

    added Eulerianmesh to module imports

--- src/pde_module/mesh/__init__.py
+++ src/pde_module/mesh/__init__.py
@@ -1,3 +1,4 @@
 from .mesh import Mesh
+from .eulerian import EulerianMesh
 from .uniformGridMesh import UniformGridMesh
 from .mesh_functions import to_pyvista,create_field,create_structured_field,create_structured_warp_field
