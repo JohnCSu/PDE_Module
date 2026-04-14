80ea39b9fe808edc61c6ef5b359c2c65f6bca5a4
John <JohnChangSu@gmail.com>
2026-04-14 15:29:56 +1000

    Added Eulerian Mesh

---
+++ src/pde_module/mesh/eulerian.py
@@ -0,0 +1,11 @@
+import numpy as np
+from pde_module.mesh import Mesh
+
+class EulerianMesh(Mesh):
+    def __init__(self, nodes, cells_connectivity, cell_types, dimension = None, float_dtype=np.float32, int_dtype=np.int32):
+        super().__init__(nodes, cells_connectivity, cell_types, dimension, float_dtype, int_dtype)
+        self.faces.get_centroids_and_normals(self.nodes)
+        self.faces.get_OwnerNeighbor()
+        self.cells.get_centroids(self.nodes)
+        self.cells.get_volumes(self.faces._cell_to_face_array_,self.faces._cell_to_face_offset_,self.faces.centroids,self.faces.normals)
+        
